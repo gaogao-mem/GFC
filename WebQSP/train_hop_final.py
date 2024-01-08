b0f2e12fc14b74565c9647c307aba3e571beacfc
gaogao-mem <2911151264@qq.com>
2024-01-08 19:13:07 +0800

    fix: change cuda number

--- WebQSP/train_hop_final.py
+++ WebQSP/train_hop_final.py
@@ -1,6 +1,6 @@
 import os
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID" 
-os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 import sys
 # sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__)))) 
 sys.path.append(os.path.abspath(os.path.join(os.getcwd(), '../'))) 
